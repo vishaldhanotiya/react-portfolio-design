79d22981c85aa17e1c9c4092073a7ccb7ee38a95
Vishal Dhanotiya <vishal_dhanotiya@Vishals-MacBook-Pro.local>
2024-06-27 00:03:32 +0530

    Change certificate folder

--- src/components/FifthSection.tsx
+++ src/components/FifthSection.tsx
@@ -4,64 +4,64 @@ const gridImage = [
   {
     id: 1,
     text: "Top Talent 2024",
-    value: require("../res/top2024.png"),
+    value: require("../res/certificate/top2024.png"),
   },
   {
     id: 2,
     text: "Top Talent 2023",
-    value: require("../res/top2023.png"),
+    value: require("../res/certificate/top2023.png"),
   },
   {
     id: 3,
     text: "Bravo - Individual Awards",
-    value: require("../res/bravo.png"),
+    value: require("../res/certificate/bravo.png"),
   },
 
   {
     id: 4,
     text: "GenAI Foundation - GenAI Assisted Coding using Amazon CdeWhisperer on FY 2023",
-    value: require("../res/genai.png"),
+    value: require("../res/certificate/genai.png"),
   },
   {
     id: 5,
     text: "Interviewers Certification Workshop",
-    value: require("../res/interview.png"),
+    value: require("../res/certificate/interview.png"),
   },
   {
     id: 6,
     text: "React Native - The Practical Guide",
-    value: require("../res/rn2024.png"),
+    value: require("../res/certificate/rn2024.png"),
   },
 
   {
     id: 7,
     text: "Master CI/CD for React Native",
-    value: require("../res/ci-cd.png"),
+    value: require("../res/certificate/ci-cd.png"),
   },
   {
     id: 8,
     text: "Fastlane for React Native: Deploy your app autonomously!",
-    value: require("../res/fastlane.png"),
+    value: require("../res/certificate/certificate/fastlane.png"),
   },
   {
     id: 9,
     text: "The Git & Github Bootcamp",
-    value: require("../res/github-bootcamp.png"),
+    value: require("../res/certificate/github-bootcamp.png"),
   },
   {
     id: 10,
     text: "The Complete React Native + Hooks Course",
-    value: require("../res/hook.png"),
+    value: require("../res/certificate/hook.png"),
   },
   {
     id: 11,
     text: "Android Studio Masterclass Conquer the Android IDE",
-    value: require("../res/android-certificate.png"),
+    value: require("../res/certificate/android-certificate.png"),
   },
   {
     id: 12,
     text: "Android Unit Testing and Test Driven Development",
-    value: require("../res/unit-testing.png"),
+    value: require("../res/certificate/unit-testing.png"),
   },
 ];
 
