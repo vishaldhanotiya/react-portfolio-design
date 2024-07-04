45108d84d0b6ba2b1919c2439c456bf572e95e3b
Vishal Dhanotiya <vishal_dhanotiya@Vishals-MacBook-Pro.local>
2024-07-04 12:15:37 +0530

    fixed lint issue

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,28 +1,5 @@
 import React from "react";
 import "./Blog.scss";
-const gridImage = [
-  {
-    text: "What is React Native?",
-    subTitle:
-      "React Native helps to develop cross-platform applications for android and iOS.",
-    value: require("../res/react-native.png"),
-  },
-  {
-    text: "Sign In with Google in the React  Expo",
-    subTitle: `Explain Google sign integration for react native expo`,
-    value: require("../res/google-sign-in.png"),
-  },
-  {
-    text: "Expo CLI vs React Native CLI",
-    subTitle: `There are two popular ways to create a React Native app- Expo CLI or React Native CLI.`,
-    value: require("../res/expo-cli.png"),
-  },
-  {
-    text: "Coding Best Practices for React Native",
-    subTitle: `The purpose of this document is to detail the code conventions.`,
-    value: require("../res/react-native.png"),
-  },
-];
 
 export default function MyBlogs() {
   return (
